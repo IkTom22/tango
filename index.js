f715565ac3b2364e390d8e342ed471e93bc872fd
Iko <ikotominaga@gmail.com>
2019-07-24 16:04:58 +1200

    loader added, sphone-land media query finished

---
+++ index.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function () {
+    const loader = document.querySelector('.loader');
+
+    const hiding = () => loader.className += " hidden";
+
+    setTimeout(hiding, 4000);
+
+
+}
+
+)
+// window.onload = () => {
+//     const loader = document.querySelector('.loader');
+
+//     const hiding = () => loader.className += " hidden";
+
+//     setTimeout(hiding, 4000);
+// }
+
