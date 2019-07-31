d347223d8f59d30f3b442ca38dfb2f28e40e1b16
Iko <ikotominaga@gmail.com>
2019-07-31 15:54:27 +1200

    slight change in loader scss

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 window.addEventListener("load", function () {
     const loader = document.querySelector('.loader');
+    const container = document.querySelector('.container');
 
     const hiding = () => loader.className += " hidden";
 
     setTimeout(hiding, 4000);
-
+    container.className += " show";
 
 }
 
